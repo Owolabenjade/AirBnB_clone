f7b29aab16a9ca0457413c77aed6346542f068d0
Benjamin <owolabenjade@gmail.com>
2024-04-07 22:00:07 -0700

    love

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Defines the FileStorage class that serializes instances to a JSON file
-and deserializes JSON file to instances.
+Defines the FileStorage class that serializes instances to a JSON
+file and deserializes JSON file to instances.
 """
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
     """Serializes instances to a JSON file & deserializes back to instances."""
@@ -18,11 +19,14 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         if obj:
-            self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+            obj_id = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[obj_id] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
-        obj_dict = {obj: self.__objects[obj].to_dict() for obj in self.__objects.keys()}
+        obj_dict = {}
+        for key in self.__objects:
+            obj_dict[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
@@ -30,12 +34,11 @@ class FileStorage:
         """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, 'r') as f:
-                objs = json.load(f)
-            for obj in objs.values():
-                cls_name = obj["__class__"]
-                del obj["__class__"]
-                if cls_name == "BaseModel":
-                    self.__objects[obj['id']] = BaseModel(**obj)
+                obj_dict = json.load(f)
+            for obj_id, obj_attrs in obj_dict.items():
+                cls_name = obj_attrs["__class__"]
+                if cls_name in globals():
+                    self.__objects[obj_id] = globals()[cls_name](**obj_attrs)
         except FileNotFoundError:
             pass
 
